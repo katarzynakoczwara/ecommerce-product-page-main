72756156ae2c6369ecbc2ea143d52b61108ea256
Katarzyna Koczwara <koczwarakkatarzyna@gmail.com>
2021-11-10 13:28:58 +0100

    Add function that add products to cart

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const photoBtns = document.querySelectorAll('.photos__btn');
 const mainPhoto = document.querySelector('.photos__main-photo');
 const minus = document.querySelector('.product-description__amount-minus');
 const plus = document.querySelector('.product-description__amount-plus');
-const amount = document.querySelector('.product-description__amount')
+const amount = document.querySelector('.product-description__amount');
+const addToCartBtn = document.querySelector('.product-description__add-to-cart-btn');
+const cartIconBtn = document.querySelector('.header__cart-icon');
+const cartContainer = document.querySelector('.cart');
 
 let counter = 1;
 let amountCounter = 0;
@@ -39,6 +42,30 @@ const increaseAmount = () => {
     amount.textContent = amountCounter;    
 }
 
+const countPrice = (price, amount) => {
+    return price * amount;
+}
+
+const addToCart = () => {
+    cartIconBtn.setAttribute('data-content', amount.textContent);
+    cartIconBtn.classList.add('show-amount');
+    const result = countPrice(125, amount.textContent);
+    cartContainer.innerHTML = `  <h4 class="cart__title">Cart</h4>
+    <div class="cart__products">
+      <div class="cart__product">
+        <img src="./images/image-product-1.jpg" alt="" class="cart__product-img">
+        <p class="cart__product-title">Autumn Limited Edition</p>
+        <div class="cart__product-price-container">
+          <p class="cart__product-price">$125.00 x </p>
+          <p class="cart__product-amount">${amount.textContent}</p>
+          <p class="cart__product-finally-price">$${result}</p>
+        </div>
+        <button class="cart__product-delete-btn"><i class="fas fa-trash-alt"></i></button>
+      </div>
+    </div>
+    <div class="cart__checkout-btn">Checkout</div>`;
+}
+
 photoBtns.forEach( btn => {
     btn.addEventListener('click', () => {
         if(btn.classList.contains('photos__next-photo-btn')) {
@@ -56,5 +83,17 @@ minus.addEventListener('click', () => {
 plus.addEventListener('click', () => {
     increaseAmount();
 });
+
+addToCartBtn.addEventListener('click', () => {
+    addToCart();
+});
+
+cartIconBtn.addEventListener('click', () => {
+    if(cartContainer.classList.contains('cart-show')) {
+        cartContainer.classList.remove('cart-show');
+    } else {
+        cartContainer.classList.add('cart-show');
+    }    
+})
 
 
